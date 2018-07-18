6fec4da8340197079b09bf23fbbee16b5a35518b
Jason Poindexter <jason@launchbadge.com>
2018-07-18 14:38:10 -0700

    Add POST /beacon/label route

--- server.py
+++ server.py
@@ -1,14 +1,54 @@
 # !/usr/bin/env python
-from flask import Flask, request
+from flask import Flask, Response, request
 from flask_sse import sse
+from jsonschema import validate, ValidationError, SchemaError
+import sys, json
+
 
 app = Flask(__name__, static_url_path='')
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')
 
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+
+@app.route('/beacon/label', methods=['GET', 'POST'])
+def label():
+    if request.method == 'POST':
+        BODY = {
+            "type": "object",
+            "properties": {
+                "label": {"type": "string"},
+                "id": {"type": "string"},
+                "active": {"type": "boolean"}
+            },
+            "required": ["label", "id", "active"]
+        }
+
+        body = request.get_json(silent=True)
+
+        if body is None:
+            return "Body is None", 422
+
+        try:
+          validate(body, BODY)
+        except ValidationError:
+            print(sys.exc_info()[0])
+            return "ValidationError", 422
+        except SchemaError:
+            print(sys.exc_info()[0])
+            return "SchemaError", 422
+        except:
+            print(sys.exc_info()[0])
+            return "Unexpected error", 422
+
+        return Response(json.dumps(body), status=200, mimetype='application/json')
+    else:
+        return "", 200
+
 
 if __name__ == '__main__':
     app.run(port="8080")
